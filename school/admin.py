740791994ebb96ce17a5e2add34048712ac924ff
Romain LEFRANC <rlefranc@lefresnoy.net>
2016-09-29 11:51:16 +0200

    admin display modif

--- school/admin.py
+++ school/admin.py
@@ -6,25 +6,43 @@ from pagedown.widgets import AdminPagedownWidget
 
 from .models import Promotion, Student, StudentApplication
 
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
+
+
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'number', 'promotion', 'graduate')
+    list_display = ('artist', 'number', 'promotion', 'graduate')
     search_fields = ('number', 'user__first_name', 'user__last_name')
 
     formfield_overrides = {
         models.TextField: {'widget': AdminPagedownWidget },
     }
 
+def output_excel(modeladmin, request, queryset):
+    # do something
+    selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+    ct = ContentType.objects.get_for_model(queryset.model)
+    return HttpResponseRedirect("/export/?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
+
+
+
+output_excel.short_description = "Sortie Excel"
+
+
+
 class StudentApplicationAdmin(admin.ModelAdmin):
+    actions = [output_excel]
 
     def _get_name(self,obj):
         return obj.artist.user.get_full_name()
 
     _get_name.short_description = "Nom"
+    list_display = ('_get_name','current_year_application_count','created_on','selected_for_interview')
 
-    list_display = ('_get_name','current_year_application_count','created_on','first_time')
+
 
 
 
 admin.site.register(Promotion)
-admin.site.register(StudentApplication,StudentApplicationAdmin)
+admin.site.register(StudentApplication, StudentApplicationAdmin )
 admin.site.register(Student, StudentAdmin)
