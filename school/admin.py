bf4b0cf78f962780e64e64ff284019fe1f103c1f
Romain LEFRANC <rlefranc@lefresnoy.net>
2016-09-29 11:51:16 +0200

    admin display modif

--- school/admin.py
+++ school/admin.py
@@ -6,13 +6,18 @@ from pagedown.widgets import AdminPagedownWidget
 from .models import Promotion, Student, StudentApplication
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'number', 'promotion', 'graduate')
+    list_display = ('artist', 'number', 'promotion', 'graduate')
     search_fields = ('number', 'user__first_name', 'user__last_name')
 
     formfield_overrides = {
         models.TextField: {'widget': AdminPagedownWidget },
     }
 
+class StudentApplicationAdmin(admin.ModelAdmin):
+    list_display = ('artist', 'selected_for_interview', 'updated_on')
+
+
+
 admin.site.register(Promotion)
-admin.site.register(StudentApplication)
+admin.site.register(StudentApplication, StudentApplicationAdmin )
 admin.site.register(Student, StudentAdmin)
