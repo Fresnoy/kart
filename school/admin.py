cc2ff5f7cf8005e3b3b1f9f3fe6b2aa76f52b4e3
McRo <rlefranc@lefresnoy.net>
2016-11-03 12:00:00 +0100

    fix conflict school

--- school/admin.py
+++ school/admin.py
@@ -6,6 +6,10 @@ from pagedown.widgets import AdminPagedownWidget
 
 from .models import Promotion, Student, StudentApplication
 
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
+
+
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('artist', 'number', 'promotion', 'graduate')
     search_fields = ('number', 'user__first_name', 'user__last_name')
@@ -14,15 +18,30 @@ class StudentAdmin(admin.ModelAdmin):
         models.TextField: {'widget': AdminPagedownWidget },
     }
 
+def output_excel(modeladmin, request, queryset):
+    # do something
+    selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+    ct = ContentType.objects.get_for_model(queryset.model)
+    return HttpResponseRedirect("/export/?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
+
+
+
+output_excel.short_description = "Sortie Excel"
+
+
+
 class StudentApplicationAdmin(admin.ModelAdmin):
+    actions = [output_excel]
 
     def _get_name(self,obj):
         return obj.artist.user.get_full_name()
 
     _get_name.short_description = "Nom"
-    list_display = ('artist', 'selected_for_interview', 'updated_on')
+    list_display = ('_get_name','current_year_application_count','created_on','selected_for_interview')
+
+
 
 
 admin.site.register(Promotion)
-admin.site.register(StudentApplication,StudentApplicationAdmin)
+admin.site.register(StudentApplication, StudentApplicationAdmin )
 admin.site.register(Student, StudentAdmin)
