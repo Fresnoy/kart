018abb458bee19ef4f698db4a4abb78056c8b9e8
McRo <rlefranc@lefresnoy.net>
2016-11-09 17:37:16 +0100

    api display medium in gallery

--- assets/serializers.py
+++ assets/serializers.py
@@ -3,11 +3,19 @@ from rest_framework import serializers
 from .models import Gallery, Medium
 
 
-class GallerySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Gallery
-
-
 class MediumSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Medium
+
+
+class GallerySerializer(serializers.HyperlinkedModelSerializer):
+
+    media = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='medium-detail'
+    )
+
+    class Meta:
+        model = Gallery
+        fields = ('label','description','media', )
