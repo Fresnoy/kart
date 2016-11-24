2a3e90707ccc7b4572968086b50b837bdc3fa6b7
McRo <rlefranc@lefresnoy.net>
2016-11-24 15:37:05 +0100

    api user serializer save and update user profile

--- people/serializers.py
+++ people/serializers.py
@@ -8,12 +8,47 @@ from .models import Artist, Staff, Organization, FresnoyProfile
 class FresnoyProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = FresnoyProfile
-#        exclude = ('user',)
+        exclude = ('user',)
 
     id = serializers.ReadOnlyField()
     birthplace_country = CountryField(default="FR")
     homeland_country = CountryField(default="FR")
     residence_country = CountryField(default="FR")
+    #user = serializers(source="user")
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name', 'email', 'profile')
+
+    profile = FresnoyProfileSerializer(required=False)
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create(**validated_data)
+        UserProfile.objects.create(**profile_data)
+        return user
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile')
+
+        # Update User data
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+
+        # Update UserProfile data
+        if not instance.profile:
+            FresnoyProfile.objects.create(user=instance, **profile_data)
+
+        #set Values for UserProfile
+        for item in profile_data:
+            value = profile_data.get(item)
+            setattr(instance.profile, item, value)
+
+        instance.save()
+        return instance
 
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
@@ -22,12 +57,6 @@ class ArtistSerializer(serializers.HyperlinkedModelSerializer):
         exclude = ('updated_on',)
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'first_name', 'last_name', 'email', "profile")
-
-    profile = FresnoyProfileSerializer()
 
 
 class StaffSerializer(serializers.HyperlinkedModelSerializer):
