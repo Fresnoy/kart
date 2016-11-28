b51195700f9aa98ac7542aae06ad56994c10e5a6
Roumain Lefranc <Roumain Lefranc>
2016-11-28 11:58:37 +0100

    Mise en place de fichiers de test

--- school/test_models.py
+++
@@ -1,9 +0,0 @@
-from .models import Promotion
-
-
-# model test
-def test_promotion():
-    starting_year = 1000
-    ending_year = 1001
-    promo = Promotion(name="test", starting_year=starting_year, ending_year=ending_year)
-    assert promo.starting_year == starting_year
---
+++ school/tests/test_restapi.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from people.models import Artist
+
+from ..models import StudentApplication
+
+class TestApplicationEndPoint(TestCase):
+    """
+    Tests concernants le endpoint des Student Application
+    """
+
+    def setUp(self):
+        self.user = User()
+        self.user.first_name = "Me"
+        self.user.last_name = "Loo"
+        self.user.username = "Tintin"
+        self.user.save()
+
+        self.artist = Artist(user = self.user, nickname="Rintintin")
+        self.artist.save()
+
+        self.application = StudentApplication(artist = self.artist)
+        self.application.save()
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_list(self):
+        """
+        Test list of applications
+        """
+
+        url = reverse('studentapplication-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_contain_user(self):
+        """
+        Test de la validité des informations
+        """
+
+        # TODO le test
+
+        pass
