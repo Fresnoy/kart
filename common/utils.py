20567abf1c1f31849ceae0c1d5d73ab0ea34b84c
McRo <rlefranc@lefresnoy.net>
2017-02-07 16:36:33 +0100

    secure unique random filepath

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,6 @@
+import os
 from django.contrib.auth.models import User
+from ifresnoy import settings
 
 
 def make_filepath(instance, filename):
@@ -6,11 +8,16 @@ def make_filepath(instance, filename):
     Generate a unique filename for any upload (fix problems with
     accents and such).
     """
-    new_filename = "{0}.{1}".format(User.objects.make_random_password(10),
-                                    filename.split('.')[-1])
 
-    return "{0}/{1}/{2}".format(
-        instance.__class__._meta.app_label,
-        instance.__class__.__name__.lower(),
-        new_filename
-    )
+    carry_on = True
+    while carry_on:
+        new_filename = "{0}.{1}".format(User.objects.make_random_password(48),
+                                        filename.split('.')[-1])
+        path = "{0}/{1}/{2}".format(
+            instance.__class__._meta.app_label,
+            instance.__class__.__name__.lower(),
+            new_filename
+        )
+        carry_on = os.path.isfile(os.path.join(settings.MEDIA_ROOT, path))
+
+    return path
