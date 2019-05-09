5eb025f48df354e139486e881c48ff093e7f71e6
McRo <rlefranc@lefresnoy.net>
2019-05-09 13:14:38 +0200

    add search by user last name to identify binome

--- school/views.py
+++ school/views.py
@@ -60,7 +60,7 @@ class StudentApplicationViewSet(viewsets.ModelViewSet):
     # serializer_class = StudentApplicationSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.SearchFilter, filters.DjangoFilterBackend, filters.OrderingFilter)
-    search_fields = ('=artist__user__username',)
+    search_fields = ('=artist__user__username','artist__user__last_name')
     filter_fields = ('application_completed',
                      'application_complete',
                      'selected_for_interview', 'remote_interview', 'wait_listed_for_interview',
