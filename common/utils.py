2bababfa816e1199b9527d3f206e539d50f17620
Romain LEFRANC <rlefranc@lefresnoy.net>
2019-11-19 17:02:38 +0100

    rewrite file upload path

--- common/utils.py
+++ common/utils.py
@@ -1,22 +1,33 @@
 import os
 import datetime
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 from kart import settings
 
 
 def make_filepath(instance, filename, prefix_folder=None):
     """Generate a unique path and filename for any upload (fix problmes with accents and such)"""
 
-    # Root directory for media
-    _r = settings.MEDIA_ROOT
-    # Context directory
-    context_dir = os.path.join(instance.__class__._meta.app_label,
-                               instance.__class__.__name__.lower())
     # Date directory
     _t = datetime.datetime.today()
-    date_dir = "{}/{}".format(_t.strftime('%Y'), _t.strftime('%m'))
-
-    # TODO : explore instance to look for user or production reference
-    # _uss = instance._meta.get_field('user')
-
-    dest_folder = os.path.join(_r, context_dir, date_dir, filename)
-    return dest_folder
+    # slug filename
+    default_folder = "{}/{}".format(_t.strftime('%Y'), _t.strftime('%m'))
+    # get file name and extension (with '.')
+    file_name, file_extension = os.path.splitext(filename)
+    # truncate long filename without accent and such
+    file_name = slugify(file_name)[:50]
+    # making unique file in path
+    carry_on = True
+    while carry_on:
+        new_filename = "{name}_{random}{extension}".format(name=file_name,
+                                                           random=User.objects.make_random_password(3),
+                                                           extension=file_extension)
+        path = "{app}/{model}/{date}/{name}".format(
+            app=instance.__class__._meta.app_label,
+            model=instance.__class__.__name__.lower(),
+            # prefix or date ( = not empty) to prevent 'app/class//file.fmt'
+            date=prefix_folder or default_folder,
+            name=new_filename
+        )
+        carry_on = os.path.isfile(os.path.join(settings.MEDIA_ROOT, path))
+    return path
