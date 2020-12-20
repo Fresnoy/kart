86fc2d438a67bb5f4e0045edab564ed093020c36
Romain <romain.lefranc@gmail.com>
2020-12-20 22:29:39 +0100

    need  context processor request template in django3

--- kart/settings.py
+++ kart/settings.py
@@ -102,6 +102,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
