9142bf0c061620cf65c779fb1365da4a19881999
François Poulain <fpoulain@metrodore.fr>
2021-05-11 15:54:29 +0200

    feat(tests): test JWT obtention and refusal

--- utils/tests/conftest.py
+++ utils/tests/conftest.py
@@ -1,7 +1,30 @@
 import pytest
 
+from . import factories
+
 
 @pytest.fixture
 def db_ready(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock() as db:
         return db
+
+
+@pytest.fixture
+def user(db_ready):
+    return factories.UserFactory()
+
+
+@pytest.fixture
+def inactive_user(db_ready):
+    return factories.UserFactory(is_active=False)
+
+
+@pytest.fixture
+def admin(db_ready):
+    return factories.AdminFactory()
+
+
+@pytest.fixture
+def joker(db_ready):
+    # return a user-like object nonexistant in db
+    return factories.UserFactory.stub()
--- utils/tests/utils.py
+++ utils/tests/utils.py
@@ -1,2 +1,17 @@
+from django.test import Client
+from django.urls import reverse
+
+
 def first(items):
     return items[0]
+
+
+def obtain_jwt_token(user):
+    c = Client()
+    url = reverse('obtain-jwt-token')
+
+    response = c.post(url, {'username': user.username, 'password': 'p4ssw0rd'})
+    assert response.status_code == 200
+    assert 'token' in response.json()
+
+    return response.json()
