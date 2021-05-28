7a7f9e8e49ddb569ba8e63831f18d5c0aacd9ba1
François Poulain <fpoulain@metrodore.fr>
2021-05-28 20:50:50 +0200

    feat(tests): add tests for common DRF API

---
+++ common/tests/test_api_v2.py
@@ -0,0 +1,55 @@
+import pytest
+
+from common.tests.conftest import *  # noqa
+from diffusion.tests.conftest import *  # noqa
+from production.tests.conftest import *  # noqa
+from utils.tests.utils import (
+    IsAuthenticatedOrReadOnlyModelViewSetMixin,
+    HelpTestForModelViewSet,
+    parametrize_user_roles,
+)
+
+
+def pytest_generate_tests(metafunc):
+    # pytest hook; called once per each test function
+    parametrize_user_roles(metafunc)
+
+
+@pytest.mark.django_db
+class TestBTBeaconViewSet(IsAuthenticatedOrReadOnlyModelViewSetMixin, HelpTestForModelViewSet):
+    viewset_name = 'common/beacon'
+
+    fixtures = ['btbeacon', 'user']
+
+    expected_list_size = 1
+    expected_fields = ['rssi_in', 'rssi_out']
+
+    mutate_fields = ['rssi_in']
+    put_fields = ['rssi_in', 'rssi_out', 'uuid', 'x', 'y', 'label']
+
+    def target(self):
+        return self.btbeacon
+
+    def requestor(self):
+        return self.user
+
+
+@pytest.mark.django_db
+class TestWebsiteViewSet(IsAuthenticatedOrReadOnlyModelViewSetMixin, HelpTestForModelViewSet):
+    viewset_name = 'common/website'
+
+    fixtures = ['website', 'user']
+
+    expected_list_size = 1
+    expected_fields = ['url', 'link', 'language', 'title_fr', 'title_en']
+
+    mutate_fields = ['title_fr']
+    put_fields = ['link', 'language', 'title_fr', 'title_en']
+    hyperlinked_fields = {'url': 'website'}
+    renamed_fields = {'link': 'url'}
+
+    def target(self):
+        return self.website
+
+    def requestor(self):
+        return self.user
