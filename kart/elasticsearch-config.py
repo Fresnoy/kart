203854dec1558509fc7f5f4f636e7e0957c9bd43
McRo <rlefranc@lefresnoy.net>
2022-09-08 16:17:29 +0200

    :arrow_up:  Vivaldi autumn 2022 (#66)
    * :arrow_up: Upgrading dependencies like Language and elastisearch 7
    * add comments
    * add people migration languagesfield
    * delete migration (name)
    * :wrench: update travis file
    * :art: code style
    * update default settings
    * :wrench: update travis file with simple elastisearch service
    * :heavy_minus_sign: remove unused dependency : elasticstack

---
+++ kart/elasticsearch-config.py
@@ -0,0 +1,49 @@
+from haystack.backends.elasticsearch7_backend import (Elasticsearch7SearchEngine, Elasticsearch7SearchBackend,
+                                                      Elasticsearch7SearchQuery)
+
+
+class Elasticsearch71SearchBackend(Elasticsearch7SearchBackend):
+    DEFAULT_SETTINGS = {
+        "settings": {
+            "index": {
+                "max_ngram_diff": 2,
+            },
+            "analysis": {
+                "analyzer": {
+                    "ngram_analyzer": {
+                        "tokenizer": "standard",
+                        "filter": [
+                            "haystack_ngram",
+                            "lowercase",
+                            "asciifolding",
+                        ],
+                    },
+                    "edgengram_analyzer": {
+                        "tokenizer": "standard",
+                        "filter": [
+                            "haystack_edgengram",
+                            "lowercase",
+                            "asciifolding",
+                        ],
+                    },
+                },
+                "filter": {
+                    "haystack_ngram": {
+                        "type": "ngram",
+                        "min_gram": 3,
+                        "max_gram": 4,
+                    },
+                    "haystack_edgengram": {
+                        "type": "edge_ngram",
+                        "min_gram": 3,
+                        "max_gram": 15,
+                    },
+                },
+            },
+        },
+    }
+
+
+class Elasticsearch71SearchEngine(Elasticsearch7SearchEngine):
+    backend = Elasticsearch71SearchBackend
+    query = Elasticsearch7SearchQuery
