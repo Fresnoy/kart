15d0ee198efe4131d6130f60e95834a6657b4e62
Romain Lefranc <rlefranc@lefresnoy.net>
2023-02-01 09:49:23 +0100

    :rotating_light: test(config) : upgrating configtest

--- kart/settings.py
+++ kart/settings.py
@@ -169,6 +169,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
