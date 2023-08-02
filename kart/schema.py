95c7e1e815cac8180e1b37d98be883ddf3618be1
Olivier Capra <olivier.capra@gmail.com>
2023-08-02 21:36:11 +0200

    perf(gql): remove Mutation mentions

--- kart/schema.py
+++ kart/schema.py
@@ -1,12 +1,15 @@
 import graphene
 
+import people.schema
+import school.schema
+import production.schema
 
-class Query(graphene.ObjectType):
+
+class Query(people.schema.Query,
+            school.schema.Query,
+            production.schema.Query,
+            graphene.ObjectType):
     pass
 
 
-class Mutation(graphene.ObjectType):
-    pass
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
