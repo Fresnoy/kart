d21b754da7b2e781f5f99b7ef3e15f5117d55c55
Romain Lefranc <rlefranc@lefresnoy.net>
2023-11-30 15:31:43 +0100

    fix(email): customize from email

--- kart/settings.py
+++ kart/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '$17%$7@*^nmx&(mb)5=o9v9if&_%s67-*^-skk!iaef3%16*12'
 PASSWORD_TOKEN = r'(?P<uidb64>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,40})'
 
 site_name = "Kartel"
-DEFAULT_FROM_EMAIL = "Le Fresnoy <selection@lefresnoy.net>"
+DEFAULT_FROM_EMAIL = FROM_EMAIL
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Application definition
