8b546f1c71aeceb0f9410c8dbadb05a10464f953
Romain Lefranc <rlefranc@lefresnoy.net>
2024-02-06 18:11:12 +0100

    test(graphql): init graphql test

---
+++ school/tests/test_graphql.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestGQLPages(TestCase):
+    """
+    TODO: do tests
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_page_studentGQL(self):
+        pass
