39276cfce94a1346672e9eecaa84484484815d36
Aurore <reynier.aurore@gmail.com>
2025-04-16 10:58:08 +0200

    feat(production): Enable queries with keywords enable queries with keywords for: -artwork -artworks -production do some units tests on keywords queries with pytest and do some refactoring according to flake8 in set_artwork_credits file

--- production/management/commands/set_artwork_credits.py
+++ production/management/commands/set_artwork_credits.py
@@ -47,8 +47,8 @@ def init():
 
     # collaborators -> credits
     setArtworkCredits(artwork, credits_text)
-    
-    print("\n".join([str(pst.staff) + " : "+ str(pst.task) for pst in artwork.staff_tasks.all()]))
+
+    print("\n".join([str(pst.staff) + " : " + str(pst.task) for pst in artwork.staff_tasks.all()]))
 
     print("FIN")
     print("---")
@@ -74,34 +74,34 @@ def searchArtwork():
 
 
 def setArtworkCredits(aw, credits):
-    
+
     # many ways detected in csv
     # staf : task \n
     # staf : name or fistname, lastname
 
-    if not "\n" in credits:
+    if "\n" not in credits:
         return False
 
     credits_arr = credits.split("\n")
     for credit in credits_arr:
 
-        if not ":" in credit:
+        if ":" not in credit:
             print("/!\\ credit n'a pas de ':'" + credit)
-            continue        
-        
+            continue
+
         # SEARCH FOR USER
         user_str = credit.split(":")[0].strip()
         print("SEARCH FOR USER : " + user_str)
         users = []
         user = False
         # "," in name mean that this is an human in DB (comme with catalog plateform dev)
-        # but mean sometimes two person for the same task Nina Guseva, Anna Collard : céramiste 
+        # but mean sometimes two person for the same task Nina Guseva, Anna Collard : céramiste
         # OR Nina Guseva et Anna Collard
-        if "," in user_str or  " et " in user_str:
+        if "," in user_str or " et " in user_str:
             str_split = user_str.split(",") if "," in user_str else user_str.split(" et ")
             first_user = str_split[0].strip()
             # is multi ? il y a un espace (nom prénom) et c'est assez long
-            if(" " in first_user and len(first_user)>3):
+            if (" " in first_user and len(first_user) > 3):
                 for u in str_split:
                     user, created = get_or_create_user(u)
                     users.append(user)
@@ -109,15 +109,15 @@ def setArtworkCredits(aw, credits):
         if not user:
             user, created = get_or_create_user(user_str)
             users.append(user)
-        
+
         # Is staff
-        staffs=[]
+        staffs = []
         for user in users:
             if user.staff_set.count() > 0:
                 staff = user.staff_set.first()
             else:
                 staff, created = Staff.objects.get_or_create(user=user)
-                setStats(staff, created)                    
+                setStats(staff, created)
             staffs.append(staff)
 
         # search for task
@@ -125,14 +125,14 @@ def setArtworkCredits(aw, credits):
         # sometimes Benjamin Griere : Graphisme 3D, Développeur 3D
         tasks = getOrCreateMultiInstancesByStr(StaffTask, 'label', task_str)
 
-        # HAVE TO 
+        # HAVE TO
         # ONE user ONE task
         # One user Multi tasks
         # Multi users One task
         # Multi user multi task -> problem
-        if (len(staffs) == 0 or 
-            len(tasks)==0 or
-            (len(staffs) > 1 and len(tasks) > 1) ):
+        if (len(staffs) == 0 or
+            len(tasks) == 0 or
+                (len(staffs) > 1 and len(tasks) > 1)):
             print("Multi user multi task : " + credit)
             for staff in staffs:
                 for task in tasks:
@@ -140,7 +140,7 @@ def setArtworkCredits(aw, credits):
                     setStats(pst, created)
                     print(pst)
                     print(task, staff)
-            
+
             continue
         elif len(staffs) == 1 and len(tasks) == 1:
             print("One user One task : " + credit)
@@ -150,7 +150,7 @@ def setArtworkCredits(aw, credits):
             setStats(pst, created)
             print(pst)
 
-        elif len(staffs) > 1 and len(tasks)==1:
+        elif len(staffs) > 1 and len(tasks) == 1:
             print("Multi user One task : " + credit)
             task = tasks[0]
             for staff in staffs:
@@ -165,8 +165,9 @@ def setArtworkCredits(aw, credits):
                 pst, created = ProductionStaffTask.objects.get_or_create(staff=staff, task=task, production=aw)
                 setStats(pst, created)
                 print(pst)
-            
+
     # END OF FOR CREDITS
+
 
 def input_choices(values):
     """
@@ -195,8 +196,9 @@ def input_choices(values):
     except Exception as e:
         print("Choix invalide", e)
         return False
-    
-# 
+
+
+#
 def get_first_last_name_from_str(str):
     str = str.strip()
     if " " in str:
@@ -213,7 +215,7 @@ def get_first_last_name_from_str(str):
 def get_or_create_user(user_str):
     # search user from usersearchutils
     # return user
-    
+
     first_name, last_name = get_first_last_name_from_str(user_str)
     print("firsname:"+first_name)
     print("lastname:"+last_name)
@@ -238,7 +240,7 @@ def get_or_create_user(user_str):
         else:
             user_search = False
             user = False
-    
+
     # try user_search if we are not in previous cases
     if not user and not user_search:
         user_search = getUserByNames(first_name, last_name)
@@ -264,15 +266,15 @@ def get_or_create_user(user_str):
             created = False
             print("KNOW User : " + str(user) + "(kart)  pour " + user_str + " (csv)")
             # print(user_search)
-                        
+
     if not user:
         username = usernamize(first_name, last_name, False)
         try:
-            user, created = User.objects.get_or_create(first_name=first_name.title(), last_name=last_name.title(), 
+            user, created = User.objects.get_or_create(first_name=first_name.title(), last_name=last_name.title(),
                                                        username=username)
         except Exception as e:
             username = usernamize(first_name, last_name, True)
-            user, created = User.objects.get_or_create(first_name=first_name.title(), last_name=last_name.title(), 
+            user, created = User.objects.get_or_create(first_name=first_name.title(), last_name=last_name.title(),
                                                        username=username)
         setStats(user, created)
         print("CREATE User : " + str(user))
@@ -294,14 +296,14 @@ def get_or_create_stafftask(task_str):
     if task_query.count() > 1:
         print("Plusieurs tâches ont été trouvées pour : " + task_str)
         task = input_choices(task_query)
-        
+
     elif task_query.count() == 1:
         task = task_query.first()
     if not task:
         task, created = StaffTask.objects.get_or_create(label=task_str.title())
-        setStats(task, created)   
+        setStats(task, created)
         print("Création TASK : " + str(task))
-    
+
     print(task)
 
     return task
@@ -311,7 +313,7 @@ def getOrCreateMultiInstancesByStr(model, attr, txt_str):
     instances = []
     txt_str = txt_str.strip()
 
-    if "," in txt_str or  " et " in txt_str:
+    if "," in txt_str or " et " in txt_str:
         str_split = txt_str.split(",") if "," in txt_str else txt_str.split(" et ")
         for s in str_split:
             instance = getOrCreateModelInstanceByStr(model, attr, s)
@@ -324,29 +326,28 @@ def getOrCreateMultiInstancesByStr(model, attr, txt_str):
 
 
 def getOrCreateModelInstanceByStr(model, attr, txt_str):
-    
+
     instance = False
 
     txt_str = txt_str.strip()
-    
-    query = model.objects.filter(**{attr+"__iexact":txt_str})
+
+    query = model.objects.filter(**{attr+"__iexact": txt_str})
     if query.count() == 0:
-        query = model.objects.filter(**{attr+"__icontains":txt_str})
-    
+        query = model.objects.filter(**{attr+"__icontains": txt_str})
+
     if query.count() > 1:
-        print(str(model) + " (csv) : "+ txt_str)
+        print(str(model) + " (csv) : " + txt_str)
         instance = input_choices(query)
-        
+
     elif query.count() == 1:
         instance = query.first()
 
     if not instance:
-        instance, created = model.objects.get_or_create(**{attr:txt_str.title()})
+        instance, created = model.objects.get_or_create(**{attr: txt_str.title()})
         setStats(instance, created)
         print("Création d'une instance" + str(model) + " : " + str(instance))
 
     return instance
-
 
 
 def multiline_input():
@@ -361,10 +362,9 @@ def multiline_input():
     return '\n'.join(contents)
 
 
-
 def setStats(value_from_model, created):
     class_name = value_from_model.__class__.__name__.lower()
     attibute = class_name + "_created" if created else class_name + "_reused"
-    if not attibute in stats:
+    if attibute not in stats:
         stats[attibute] = []
     stats[attibute].append(value_from_model)
