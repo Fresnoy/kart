d7b8619827558111d0e1e935c6b96c024c65072f
Romain Lefranc <rlefranc@lefresnoy.net>
2025-09-03 15:42:43 +0200

    fix(admin): bug artwork without event (phantom prod)

--- production/admin.py
+++ production/admin.py
@@ -1,3 +1,4 @@
+from math import e
 from django.contrib import admin
 from django.db import models
 
@@ -119,7 +120,9 @@ class ArtworkAdmin(admin.ModelAdmin):
     get_authors.short_description = "Artist(s)"
 
     def get_diffusions(self, obj):
-        return ", ".join([event.__str__() for event in obj.events.all()])
+        if hasattr(obj, 'events'):
+            return ", ".join([event.__str__() for event in obj.events.all()])
+        return ""
 
     get_diffusions.short_description = "Diffusion(s)"
     get_diffusions.admin_order_field = 'events'
